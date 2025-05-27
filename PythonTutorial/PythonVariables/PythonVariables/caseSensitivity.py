b8357f539b89f1e0c9522a9df761f504ba2ff61a
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-27 18:23:27 +0100

    Created another example file

---
+++ PythonTutorial/PythonVariables/PythonVariables/caseSensitivity.py
@@ -0,0 +1,7 @@
+# Variable names are case-sensitive.
+a = 4
+A = "Sally"
+# A will not overwrite a
+
+print(a)
+print(A)
---
+++ PythonTutorial/PythonVariables/PythonVariables/quotes.py
@@ -0,0 +1,6 @@
+# String variables can be declared either by using single or double quotes:
+x = "John"
+print(x)
+# is the same as
+x = 'John'
+print(x)
