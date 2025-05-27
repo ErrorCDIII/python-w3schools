88bd2aa458c3e73505a2662691eaa4252d2ffb2e
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-27 20:46:11 +0100

    Updated variable naming example file

---
+++ PythonTutorial/PythonVariables/VariableNames/example.py
@@ -0,0 +1,19 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Python variables are case-sensitive
+
+# Multi Words variable Names
+
+# Camel Case
+myVariableName = "John"
+
+# Pascal Case
+MyVariableName = "John"
+
+# Snake Case
+my_variable_name = "John"
