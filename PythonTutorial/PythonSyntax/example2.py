ad5e8b958feb164df5ca372134164b03f71aab6e
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-27 18:00:35 +0100

    Commented example file

--- PythonTutorial/PythonSyntax/example2.py
+++ PythonTutorial/PythonSyntax/example2.py
@@ -3,3 +3,4 @@ if 5 > 2:
 if 5 > 2:
         print("Five is greater than two!")
         # Both versions are viable
+        # You have to use the same number of spaces in the same block of code, otherwise Python will give you an error
