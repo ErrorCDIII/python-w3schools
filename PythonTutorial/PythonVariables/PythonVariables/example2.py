362e7e6fc977fe2b6bb48a481f579e49c09ffbc9
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-27 18:18:06 +0100

    Created another example file

---
+++ PythonTutorial/PythonVariables/PythonVariables/example2.py
@@ -0,0 +1,5 @@
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4           # x is of the type int
+print(x)
+x = "Sally"     # x is now of type str
+print(x)
