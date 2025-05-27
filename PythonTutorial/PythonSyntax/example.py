5dd7f3fb5d0678b0421406da0944f243a07b5a04
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-27 17:56:46 +0100

    Commented example file

--- PythonTutorial/PythonSyntax/example.py
+++ PythonTutorial/PythonSyntax/example.py
@@ -1,3 +1,8 @@
 # In Pyhthon, indentation has 
 if 5 > 2:
     print("Five is greater than two!")
+    # Not using indentation in this case generates an error
+#       File "/home/kali/Documents/Coding/Python/python-w3schools/PythonTutorial/PythonSyntax/example.py", line 3
+#     print("Five is greater than two!")
+#     ^^^^^
+# IndentationError: expected an indented block after 'if' statement on line 2
