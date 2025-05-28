86b92110a7a998ddfa5c287d86c0d3d9bd99d08c
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-28 17:46:58 +0100

    Created type conversion file

---
+++ PythonTutorial/PythonNumbers/typeConversion.py
@@ -0,0 +1,23 @@
+# You can convert from one type to another with the int(), float(), and complex() methods
+x = 1       # int
+y = 2.8     # float
+z = 1j      # complex
+
+# convert from int into float
+a = float(x)
+
+# convert from float into int
+b = int(y)
+
+# convert from int into complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Note: You cannot convert complex numbers into another number type.
