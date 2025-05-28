5656b7f82eb1ff860ebee350160daccf0ff9a335
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-28 17:57:19 +0100

    Created example file

---
+++ PythonTutorial/PythonCasting/example.py
@@ -0,0 +1,24 @@
+# Integers
+x = int(1)      # x will be 1
+y = int(2.8)    # y will be 2
+z = int("3")    # z will be 3
+
+print(x)
+print(y)
+print(z)
+
+# Floats
+a = float(1)        # a will be 1.0
+b = float(2.8)      # b will be 2.8
+c = float("3")      # c will be 3.0
+d = float("4.2")    # d will be 4.2
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Strings
+varA = str("s1")    # varA will be 's1'
+varB = str(2)       # varB will be '2'
+varC = str(3.0)     # varC will be '3.0'
