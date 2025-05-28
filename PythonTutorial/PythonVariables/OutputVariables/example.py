c8b58c4b1986049a6d30f0385916cbb880354431
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-28 16:47:59 +0100

    Created example file

---
+++ PythonTutorial/PythonVariables/OutputVariables/example.py
@@ -0,0 +1,26 @@
+x = "Python is awesome"
+print(x)
+
+# In the print() function, you output multiple variables, separated by a comma
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# You can also use the + operator to output multiple variables
+a = "Python "
+b = "is "
+c = "awesome"
+print(a + b + c)
+
+# For numbers, the + character works as a mathematical operator
+foo = 5
+bar = 10
+print(foo + bar)
+# Data types must be compatible
+
+# The best way to output multiple variables in the print() function is to separate them with commas, 
+# which even support different data types
+varX = 5
+varY = "John"
+print(varX, varY)
