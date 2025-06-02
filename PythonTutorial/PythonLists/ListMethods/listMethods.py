ecd70b9aa4ac8ea76d48a9f97db275a20fab1e3d
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-02 13:04:41 +0100

    Created list methods related file

---
+++ PythonTutorial/PythonLists/ListMethods/listMethods.py
@@ -0,0 +1,17 @@
+# Method        Description
+# append()      Adds an element at the end of the list
+# clear()       Removes all the elements from the list (empty)
+# copy()        Returns a copy of the list
+# count()       Returns the number of elements with specified value
+# extend()      Add elements of a list (or any iterable) to the end of the current list      
+# index()       Returns the index of the first element with the specified value
+# insert()      Adds an element at the specified position
+# pop()         Removes the elemento at the specified position
+# remove()      Removes the item with the specified value 
+# reverse()     Reverses the order of the list
+# sort()        Sorts the list
+
+list1 = [1,2,3]
+print(list1)
+list1.clear()
+print(list1)
