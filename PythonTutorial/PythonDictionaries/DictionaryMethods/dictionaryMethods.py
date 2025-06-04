da18da477d53ad372696bdc8a2f00a1c18bcdf74
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-04 14:27:03 +0100

    Created dictionary methods related file

---
+++ PythonTutorial/PythonDictionaries/DictionaryMethods/dictionaryMethods.py
@@ -0,0 +1,11 @@
+# Method          Description
+# clear()         Removes all elements from the dictionary
+# copy()          Returns a copy of the dicitonary
+# fromkeys()      Returns a dictionary with the specified keys and value
+# get()           Returns the value of the specified key
+# items()         Returns a list containing a tuple for each key:value pair         
+# keys()          Returns a list containing the dictionary's keys
+# pop()           Removes the element with the specified key
+# popitem()       Removes the last inserted key:value pair
+# update()        Returns the vlaue of the specified key. If the key does nto exit: insert the key, with the specified value
+# values()        Updates the dictionary with the specified key:value pairs
