44fc41983eaad8aad179d785b74520bba9ecee55
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-05 11:56:03 +0100

    Updated match cases related  file

--- PythonTutorial/PythonMatch/pythonMatch.py
+++ PythonTutorial/PythonMatch/pythonMatch.py
@@ -48,4 +48,25 @@ match day:
 # The value _ will always match, so it is important to place it as the last case to make it behave as a default case
 
 # Combine Values
+# Use the pipe character | as on operator in the case evaluation to check for more than one value match in one case:
 
+print()
+
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print("I lovve weekends!")
+
+# If Statements as Guards
+# You can add if statements in the case evaluation as an extra condition-check
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A 2eekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
