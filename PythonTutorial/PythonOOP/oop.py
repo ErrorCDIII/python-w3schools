75c685e44afb67b610555c29ba1d187a97520ad7
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-06 13:46:57 +0100

    Created OOP related file

---
+++ PythonTutorial/PythonOOP/oop.py
@@ -0,0 +1,24 @@
+# What is OOP?
+
+# OOP stands for Object-Oriented Programming
+# In OOP, you create objects.
+# Each object can store data and have functions that 
+# work with that data
+
+# Advantages of OOP
+
+#   Provides a clear structure to programs
+#   Makes code easier to maintain, reuse, and debug
+#   Helps keep you code DRY (Don't Repeat Yourself)
+#   Allows you to build reusable applications with less code
+
+# What are Classes and Objects?
+# A class defines what an object should look like, and 
+# an object is created on said class.
+
+# Class   Objects
+# Fruit   Apple, Banana, Mango
+# Car     Volvo, Audi, Toyota
+
+# When you create an object from c lass, it inherits 
+# variables and functions defined inside that class
