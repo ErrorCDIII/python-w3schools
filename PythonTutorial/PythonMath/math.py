c4b44066dae9e57d308920ed3d2829554cc2b9c3
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-09 13:31:59 +0100

    Created math module related file

---
+++ PythonTutorial/PythonMath/math.py
@@ -0,0 +1,66 @@
+# Math
+
+# Python has a set of built-in math functions, including
+# an extensive math module, that allows you to perform
+# mathematical tasks on numbers
+
+# Built-in Math Functions
+
+# The min() and max() functions can be used to find the 
+# lowest or highest value in an iterable:
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# The abs() function returns the absolute (positive)
+# value of the specified number
+print()
+
+x = abs(-7.25)
+
+print(x)
+
+# The pow(x, y) function returns the value of the x 
+# to the power of y (x^y)
+
+print()
+
+x = pow(4, 3)
+
+print(x)
+
+# The Math Module
+
+import math
+
+x = math.sqrt(64)
+
+print()
+
+print(x)
+
+# The math.ceil() metho rounds a number upwards to it's
+# neares integer and the math.floor() method rounds a
+# number downards to it's nearest integer, and returns the result
+import math
+
+print()
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+# The math.pi constant, returns the value of PI
+
+import math
+
+print()
+
+x = math.pi
+
+print(x)
