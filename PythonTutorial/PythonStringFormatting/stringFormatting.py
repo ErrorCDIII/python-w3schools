72a89e6e370c4e305f7b55c37dc6898cb73df463
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-11 14:11:35 +0100

    Updated F-String related file

--- PythonTutorial/PythonStringFormatting/stringFormatting.py
+++ PythonTutorial/PythonStringFormatting/stringFormatting.py
@@ -116,3 +116,119 @@ print(txt)
 # other than .2f
 
 # Example: use a comma as a thousand seperator
+
+print()
+
+price = 59000
+txt = f"The price is {price:,} dollars"
+print(txt)
+
+# Formatting Types
+# :<  Left aligns the result
+# :>  Right aligns the result
+# :^  Center aligns the result
+# :=  Places sign to the left most position
+# :+  Use the plus sign to indicate if the result is positive or 
+#     negative
+# :-  Use the minus sign to indicate negative values only
+# :   Use a space to insert an extra space before positive numbers
+# :,  Use a comma as a thousand separator
+# :_  Use underscore as a thousand separator
+# :b  Binary format
+# :c  Converts the value into the corresponding Unicode character
+# :d  Decimal format
+# :e  Scientific format, with a lower case e
+# :E  Scientific format, with a upper case e
+# :f  Fix point number format
+# :F  Fix point number format, in uppercase format
+# :g  General format
+# :G  General format (using upper case E for scientific notations)
+# :o  Octal format
+# :x  Hex format, lower case
+# :X  Hex format, upper case
+# :n  Number format
+# :%  Percentage format
+
+# String format()
+
+# Before Python 3.6, we used the format() method 
+# It can still be used and it also uses curly brackets as placeholders
+
+# Example: add a placeholder where you want to display the price
+
+print()
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+# You can add parameters inside the curly brackets to specify 
+# how to convert the value
+
+# Example: format the price to be displayed as a number with two decimals
+print()
+
+txt = "The price is {:.2f} dollars"
+print(txt.format(price))
+
+# Multiple Values
+
+# If you want to use more values, just add more values to the
+# format() method 
+
+print()
+
+itemno = 30
+count = 5
+
+print(txt.format(price, itemno, count))
+
+# And add more placeholders
+
+print()
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Index Numbers
+
+# You can use index numbers (a number inside the curlt brackets)
+# {0} to be sure the values are placed inthe correct placeholders
+
+# Example:
+
+print()
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Alse, if you want to refer to the same value more than once,
+# use the index number:
+
+# Example:
+
+print()
+
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
+# Named Indexes
+
+# You can also use named indexes by entering a name inside the
+# curly brackets {carname}, but then you must use names when you
+# pass the parameter values txt.format(carname = "Ford")
+
+# Example:
+
+print()
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
