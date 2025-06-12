7d1e683cd8684d59452e99b3b156c5b383574eba
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-12 15:32:09 +0100

    Created intro file

---
+++ PythonModules/NumPy/NumPyTutorial/NumPyGettingStarted/gettingStarted.py
@@ -0,0 +1,25 @@
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+# NumPy is usually imported under the np alias
+
+import numpy as np
+
+print()
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+# Checking NumPy Version
+
+# The version string is stored under __version__ attribute
+
+import numpy as np
+
+print()
+
+print(np.__version__)
