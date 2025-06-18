4ebef4594869f40a0ac9515cb80ea7cad07df427
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-18 14:49:43 +0100

    Updated exponential distribution related file

--- PythonModules/NumPy/NumPyRandom/ExponentialDistribution/exponentialDistribution.py
+++ PythonModules/NumPy/NumPyRandom/ExponentialDistribution/exponentialDistribution.py
@@ -1,2 +1,29 @@
 # Exponential Distribution
 
+# Is used used for describing time till event
+
+# It has two parameters:
+#   scale - inverse of rate (lam in poisson distribution) - defaults to 1.0
+#   size - the shape of the returned array
+
+from numpy import random
+
+x = random.exponential(scale=2, size=(2, 3))
+
+print(x)
+
+# Visualization of Exponencial Distribution
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.exponential(size=1000), kind="kde")
+
+plt.show() 
+
+# Relatioon Between Poisson and Exponencial Distribution
+
+# Poisson distribution deals with number of occurences of an event in a 
+# time period whereas exponential distribution deals with the time between 
+# these events
